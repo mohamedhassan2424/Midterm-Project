5caceee1fa4270cc50a3787e7845944e76866384
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-09-26 20:16:45 +0000

    comitting code

--- server.js
+++ server.js
@@ -123,6 +123,12 @@ app.get("/creatingQuiz", (req,res)=>{
 
   res.render("creatingQuizPage",templateVars)
 })
+app.get("/quizCreatedSuccesfully", (req,res)=>{
+  const currentSession = req.session.userId
+  const existsingUser=usersDatabase[currentSession]
+  const templateVars ={user: existsingUser}
+  res.render("quizCreatedSuccesfully",templateVars)
+})
 app.post("/register", (req,res)=>{
   const generatedId = Math.random().toString(36).substring(2, 8);
   const { email, password,firstName,lastName } = req.body;

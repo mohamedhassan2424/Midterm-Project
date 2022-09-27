7a76dc6f23ac784205f55d3ffc18460606d95e8e
Jacqline <jacqlinegeng@gmail.com>
2022-09-26 23:51:53 -0400

    refactored ejs and server files

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
 // load .env data into process.env
-require('dotenv').config();
+require("dotenv").config();
 
 // Web server config
-const sassMiddleware = require('./lib/sass-middleware');
-const express = require('express');
-const morgan = require('morgan');
-const cookieSession = require('cookie-session')
+const sassMiddleware = require("./lib/sass-middleware");
+const express = require("express");
+const morgan = require("morgan");
+const cookieSession = require("cookie-session");
 const bcrypt = require("bcryptjs");
-const { getUserByEmail} = require("./helperFunction")
+const { getUserByEmail } = require("./helperFunction");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  '/styles',
+  "/styles",
   sassMiddleware({
-    source: __dirname + '/styles',
-    destination: __dirname + '/public/styles',
+    source: __dirname + "/styles",
+    destination: __dirname + "/public/styles",
     isSass: false, // false => scss, true => sass
   })
 );
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const userApiRoutes = require('./routes/users-api');
-const widgetApiRoutes = require('./routes/widgets-api');
-const usersRoutes = require('./routes/users');
-const { password } = require('pg/lib/defaults');
+const userApiRoutes = require("./routes/users-api");
+const widgetApiRoutes = require("./routes/widgets-api");
+const usersRoutes = require("./routes/users");
+const { password } = require("pg/lib/defaults");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
-app.use('/api/widgets', widgetApiRoutes);
-app.use('/users', usersRoutes);
+app.use("/api/users", userApiRoutes);
+app.use("/api/widgets", widgetApiRoutes);
+app.use("/users", usersRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -53,104 +53,117 @@ app.use(
     keys: ["secretKey1", "secretKey2"],
   })
 );
+
 const usersDatabase = {
   userID: {
     id: "userID",
     email: "email@example.com",
     password: "user-typed-password",
     firstName: "First Name",
-    lastName:"Last Name"
-  }
+    lastName: "Last Name",
+  },
 };
+
 const questionText = {
   userID: {
-    id:"UserId",
-    firstQuestion:"Question Text",
-    answerOne:"Possible answer one",
-    answerTwo:"Possible answer one",
-    answerThree:"Possible answer one",
-    answerFour:"Possible answer one"
-  }
-}
-app.get('/', (req, res) => {
-  res.render('index');
+    id: "UserId",
+    firstQuestion: "Question Text",
+    answerOne: "Possible answer one",
+    answerTwo: "Possible answer one",
+    answerThree: "Possible answer one",
+    answerFour: "Possible answer one",
+  },
+};
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get("/mainPage", (req,res)=>{
-  const currentSession = req.session.userId
-  const existsingUser=usersDatabase[currentSession]
-  const templateVars ={user: existsingUser}
-  res.render("mainPage",templateVars)
-})
-app.get("/login", (req,res)=>{
-  const currentSession = req.session.userId
-  const existsingUser=usersDatabase[currentSession]
-  const templateVars ={user: existsingUser}
-  console.log("existsingUser",existsingUser)
-  if(existsingUser){
-    return res.redirect('/mainPage')
-  }
-  res.render("loginPage",templateVars)
-})
-app.post("/login", (req,res)=>{
-  const { email, password } = req.body;
-  const currentSession = req.session.userId
-  const existsingUser=usersDatabase[currentSession]
-  const loggedInUser= getUserByEmail(email, usersDatabase);
+app.get("/main-page", (req, res) => {
+  const currentSession = req.session.userId;
+  const existsingUser = usersDatabase[currentSession];
+  const templateVars = { user: existsingUser };
+  res.render("main-page", templateVars);
+});
 
-  const comparingThePassword = bcrypt.compareSync(password, loggedInUser.password)
-  console.log("comparingThePassword",comparingThePassword)
-  if(!comparingThePassword){
-    res.send('Invlaid Password, Please Try and re-enter your password again.')
+app.get("/login", (req, res) => {
+  const currentSession = req.session.userId;
+  const existsingUser = usersDatabase[currentSession];
+  const templateVars = { user: existsingUser };
+  if (existsingUser) {
+    return res.redirect("/main-page");
+  }
+  res.render("login-page", templateVars);
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const currentSession = req.session.userId;
+  const existsingUser = usersDatabase[currentSession];
+  const loggedInUser = getUserByEmail(email, usersDatabase);
+
+  const comparingThePassword = bcrypt.compareSync(
+    password,
+    loggedInUser.password
+  );
+  console.log("comparingThePassword", comparingThePassword);
+  if (!comparingThePassword) {
+    res.send("Invlaid Password, Please Try and re-enter your password again.");
   }
   if (comparingThePassword && loggedInUser) {
     req.session.userId = loggedInUser.id;
-    return res.redirect("/mainPage");
+    return res.redirect("/main-page");
   }
-  //res.render("loginPage")
-})
+});
 
-app.post("/logout", (req,res)=>{
-  req.session = null
-  res.redirect("/mainPage")
-})
-app.get("/register", (req,res)=>{
-  const currentSession = req.session.userId
-  console.log("Current Session", req.session.userId)
-  const existsingUser=usersDatabase[currentSession]
-  const templateVars ={user: existsingUser}
-  console.log("existsingUser",existsingUser)
-  if(existsingUser){
-    return res.redirect("/mainPage")
+app.post("/logout", (req, res) => {
+  req.session = null;
+  res.redirect("/main-page");
+});
+
+app.get("/register", (req, res) => {
+  const currentSession = req.session.userId;
+  console.log("Current Session", req.session.userId);
+  const existsingUser = usersDatabase[currentSession];
+  const templateVars = { user: existsingUser };
+  console.log("existsingUser", existsingUser);
+  if (existsingUser) {
+    return res.redirect("/main-page");
   }
-  res.render("registrationPage",templateVars)
-})
+  res.render("registration-page", templateVars);
+});
 
-app.get("/creatingQuiz", (req,res)=>{
-  const currentSession = req.session.userId
-  const existsingUser=usersDatabase[currentSession]
-  const templateVars ={user: existsingUser}
+app.get("/creating-quiz-page", (req, res) => {
+  const currentSession = req.session.userId;
+  const existsingUser = usersDatabase[currentSession];
+  const templateVars = { user: existsingUser };
 
-  res.render("creating-quiz-page",templateVars)
-})
-app.post("/creatingQuiz", (req,res)=>{
-  const currentSession = req.session.userId
-  const existsingUser=usersDatabase[currentSession]
-  const {question,firstAnswer,secondAnswer,thirdAnswer,fourthAnswer} = req.body
-  console.log("QUESITONNNN",question)
+  res.render("creating-quiz-page", templateVars);
+});
+
+app.post("/creating-quiz-page", (req, res) => {
+  const currentSession = req.session.userId;
+  const existsingUser = usersDatabase[currentSession];
+  const { question, firstAnswer, secondAnswer, thirdAnswer, fourthAnswer } =
+    req.body;
+  console.log("QUESITONNNN", question);
   const generatedId = Math.random().toString(36).substring(2, 8);
-  questionText[generatedId]= {
-    id:generatedId,
-    firstQuestion:question,
-    answerOne:firstAnswer,
-    answerTwo:secondAnswer,
-    answerThree:thirdAnswer,
-    answerFour:fourthAnswer
-  }
-  console.log("QuestionText",questionText)
-  const templateVars ={user: existsingUser,questionObject:questionText[generatedId]}
-  res.render("quizCreatedSuccesfully",templateVars)
-})
+  questionText[generatedId] = {
+    id: generatedId,
+    firstQuestion: question,
+    answerOne: firstAnswer,
+    answerTwo: secondAnswer,
+    answerThree: thirdAnswer,
+    answerFour: fourthAnswer,
+  };
+  console.log("QuestionText", questionText);
+  const templateVars = {
+    user: existsingUser,
+    questionObject: questionText[generatedId],
+  };
+  res.render("quiz-created", templateVars);
+});
+
 /*app.get("/quizCreatedSuccesfully", (req,res)=>{
   const currentSession = req.session.userId
   const existsingUser=usersDatabase[currentSession]
@@ -158,40 +171,52 @@ app.post("/creatingQuiz", (req,res)=>{
   const templateVars ={user: existsingUser,questionObject:questionText[generatedId]}
   res.render("quizCreatedSuccesfully",templateVars)
 })*/
-app.post("/register", (req,res)=>{
+
+app.post("/register", (req, res) => {
   const generatedId = Math.random().toString(36).substring(2, 8);
-  const { email, password,firstName,lastName } = req.body;
-  const newhashedPassword = bcrypt.hashSync(password, 10)
+  const { email, password, firstName, lastName } = req.body;
+  const newhashedPassword = bcrypt.hashSync(password, 10);
   if (email.length === 0) {
-    return res.send("400 Status Code : Please type in an accepatable Email with substantial chracter length to continue with registering.");
+    return res.send(
+      "400 Status Code : Please type in an accepatable Email with substantial chracter length to continue with registering."
+    );
   }
   if (password.length === 0) {
-    return res.send("400 Status Code : Please type in an accepatable password with substantial chracter length  continue with registering");
+    return res.send(
+      "400 Status Code : Please type in an accepatable password with substantial chracter length  continue with registering"
+    );
   }
   if (firstName.length === 0) {
-    return res.send("400 Status Code : Please type in an accepatable value for FIRST Name button with substantial chracter length  continue with registering");
+    return res.send(
+      "400 Status Code : Please type in an accepatable value for FIRST Name button with substantial chracter length  continue with registering"
+    );
   }
   if (lastName.length === 0) {
-    return res.send("400 Status Code : Please type in an accepatable value for LAST Name button with substantial chracter length continue with registering");
+    return res.send(
+      "400 Status Code : Please type in an accepatable value for LAST Name button with substantial chracter length continue with registering"
+    );
   }
   if (getUserByEmail(email, usersDatabase)) {
-    return res.send("Staus Code 400. Email type in already exist in the Database. Please type in a Different database");
+    return res.send(
+      "Staus Code 400. Email type in already exist in the Database. Please type in a Different database"
+    );
   }
-  usersDatabase[generatedId]= {
-    id:generatedId,
-    email:email,
-    password:newhashedPassword,
-    firstName:firstName,
-    lastName:lastName
-  }
-  req.session.userId=generatedId;
-  console.log(usersDatabase)
-  console.log("REQ-session-Id",req.session.userId)
+  usersDatabase[generatedId] = {
+    id: generatedId,
+    email: email,
+    password: newhashedPassword,
+    firstName: firstName,
+    lastName: lastName,
+  };
+  req.session.userId = generatedId;
+  console.log(usersDatabase);
+  console.log("REQ-session-Id", req.session.userId);
   //console.log(email)
   //console.log(password)
   //console.log(newhashedPassword)
-  res.redirect("/mainPage")
-})
+  res.redirect("/main-page");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });

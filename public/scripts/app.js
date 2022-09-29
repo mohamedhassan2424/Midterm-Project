83dd70cb9cb262abae45368c4af5fed0551dd24d
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-09-29 17:32:59 +0000

    fixing the bugs on the jquery part

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,11 @@
 // Client facing scripts here
-let count = 0;
+
 $(function() {
-  $('.add-question').on('click', function( e ) {
-    //count++;
-    console.log("COUNT", count)
-    if(count > 4){
-      return false;
-    }
+  $('.add-question').on('click', function( e ) 
     console.log("click");
       e.preventDefault();
       $('<li/>').addClass( 'new-text-div' )
-      .html( $(`<textarea style ="background-color:#FFCCCB" placeholder="Possible Wrong Answer" type="textbox" name ="possiblefirstAnswer${count}" />`).addClass( 'someclass' ) )
+      .html( $(`<textarea style ="background-color:#FFCCCB" placeholder="Possible Wrong Answer" type="textbox" name ="possiblefirstAnswer" />`).addClass( 'someclass' ) )
       .append( $('<button/>').addClass( 'remove' ).text( 'Remove' ) )
       .insertBefore( this );
   });

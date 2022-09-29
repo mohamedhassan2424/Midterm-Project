0e752fdad5304d8345cf31d0c3771b6109f1933c
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-09-29 15:52:03 +0000

    implenting the jquery into the database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,20 @@
 // Client facing scripts here
-
+let count = 0;
 $(function() {
   $('#add-question').on('click', function( e ) {
+    count++;
+    console.log("COUNT", count)
+    if(count > 4){
+      return false;
+    }
+    let countValue = 0
+    countValue.innerHTML= count
+    console.log("countValue.innerHTML",countValue.innerHTML)
     console.log("click");
       e.preventDefault();
-      $('<div/>').addClass( 'new-text-div' )
-      .html( $('<input type="textbox"/>').addClass( 'someclass' ) )
+      $('<li/>').addClass( 'new-text-div' )
+     
+      .html( $(`<textarea style ="background-color:#FFCCCB" placeholder="Possible Wrong Answer" type="textbox" name =${} />`).addClass( 'someclass' ) )
       .append( $('<button/>').addClass( 'remove' ).text( 'Remove' ) )
       .insertBefore( this );
   });

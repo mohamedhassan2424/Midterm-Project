4b5d046e270425c3a59886fff1cd92f5c29ec7e1
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-09-30 00:10:07 +0000

    fixing various bugs in the code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,23 @@
 // Client facing scripts here
 
 $(function() {
-  $('.add-question').on('click', function( e ) 
+  $('.add-question').on('click', function( e ) {
     console.log("click");
       e.preventDefault();
+      //const continerElement = $(".container").closest(".correctAnswer")
+      console.log($(this))
+      //attr("name")
+       //console.log( continerElement)
       $('<li/>').addClass( 'new-text-div' )
-      .html( $(`<textarea style ="background-color:#FFCCCB" placeholder="Possible Wrong Answer" type="textbox" name ="possiblefirstAnswer" />`).addClass( 'someclass' ) )
+      .html( $(`<textarea style ="background-color:#FFCCCB" class="textArea" placeholder="Possible Answer" name="possibleOtherAnswer">`).addClass( 'someclass' ) )
       .append( $('<button/>').addClass( 'remove' ).text( 'Remove' ) )
       .insertBefore( this );
   });
   $(document).on('click', 'button.remove', function( e ) {
       e.preventDefault();
-      $(this).closest( 'div.new-text-div' ).remove();
+      $(this).closest( 'li.new-text-div' ).remove();
   });
 
+});
+
+// creating the jquery for the dynamic changes but was able to refernce it back to the database
